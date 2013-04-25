eca26200e3ee542305de0d493045d40f0f777cff
Greg Sterndale <gsterndale@gmail.com>
2013-04-25 11:51:39 -0400

    New Rails app
    $ rails new timecard
    $ git init
    $ git add .
    $ git commit -m 'THIS MESSAGE'
    
    # Note git commands will be excluded from future commit messages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timecard::Application.config.secret_token = '37a95f6d903414ef602c19b8f32845913685b86b261af5964a50d9a8e701406de450559f62a73737a7d97f6dd6f997272e1f3ae65e14452ef42ca258aec015ac'
