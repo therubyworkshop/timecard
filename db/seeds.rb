12841693d27ab3e465f09548864a4340c0baff2b
Greg Sterndale <gsterndale@gmail.com>
2013-04-27 03:00:30 -0400

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+{
+  "Google"    => 123.45,
+  "Apple"     => 999.99,
+  "Microsoft" => 555
+}.each do |name, rate|
+  card = Card.where(name: name).first_or_create!({rate: rate}, without_protection: true)
+  %w(LP SJ BG).each do |initials|
+    Stamp.where(card_id: card.id, initials: initials).first_or_create!({
+        duration:     rand(10),
+        completed_at: Time.zone.now - rand(30).days
+      }, without_protection: true
+    )
+  end
+end
